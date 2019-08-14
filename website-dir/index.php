32181d263650dfd7aa1d02e21511cc074a599cec
Manuel Köllner <KOE18463@spengergasse.at>
2019-08-15 01:41:31 +0200

    index.php head and structure

--- website-dir/index.php
+++ website-dir/index.php
@@ -5,4 +5,36 @@
  * Date: 15.08.2019
  * Time: 00:37
  */
-phpinfo();
+error_reporting(0);
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <!-- meta -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Crispy Website">
+    <meta name="keywords" content="Crispy diner chicken">
+    <meta name="author" content="Manuel Koellner">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- extern stylesheets -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+    <link rel="stylesheet" href="app/css/bootstrap.css">
+    <!-- costume stylesheets -->
+    <link rel="stylesheet" href="app/css/base.css">
+    <!-- scripts -->
+    <script src="app/js/base.js"></script>
+    <script src="app/js/bootstrap.bundle.js"></script>
+    <script src="app/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <!-- favicon -->
+
+    <title>Manuel Köllner</title>
+</head>
+<body>
+<header></header>
+<main></main>
+<footer></footer>
+</body>
+</html>

4bca50e94fbe9f448f31467ab76dd63f355344af
3CHIF_Köllner_Manuel <koe18463@spengergasse.at>
2019-08-19 09:24:09 +0000

    userHome.php: navbar

--- website-dir/app/userHome.php
+++ website-dir/app/userHome.php
@@ -30,6 +30,31 @@ session_start();
     <title>Mein Barman</title>
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggle" type="button" data-toggle="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder"Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</header>
 <main>
     <section class="mw-100 p-0 bg-dark text-light" style="height: calc(100vh - 70vh)">
         <div class="p-5">
